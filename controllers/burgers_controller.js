83e5cb8ab01bad76cf1b19df3aa68ff9a4d630e8
Mike Gishey <mgishey56@gmail.com>
2019-11-04 12:35:37 -0700

    index route working

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,10 +1,20 @@
 var express = require("express");
+var router = express.Router();
 
 // Import the model (burger.js) to use its database function
-
 var burger = require("../models/burger.js");
 
-// Create routes and set up logic
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res){
+    burger.all(function(data){
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        //res.render("index", hbsObject);
+    });
+});
+
 
 
 
